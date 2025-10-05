83b00e8c0e1f3f306d31913ab8c0081f3ba8db88
muradilova-dev <muradilova_r@iuca.kg>
2025-10-05 22:10:29 +0600

    Created app URLs and included in main URLs

---
+++ blog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from .views import (
+    CategoryListView, CategoryDetailView, CategoryCreateView,
+    CategoryUpdateView, CategoryDeleteView,
+    PostListView, PostDetailView, PostCreateView,
+    PostUpdateView, PostDeleteView
+)
+
+urlpatterns = [
+    path('categories/', CategoryListView.as_view(), name='category_list'),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('categories/create/', CategoryCreateView.as_view(), name='category_create'),
+    path('categories/<int:pk>/update/', CategoryUpdateView.as_view(), name='category_update'),
+    path('categories/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category_delete'),
+
+    path('posts/', PostListView.as_view(), name='post_list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/create/', PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+]
