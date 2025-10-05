04ae0b9ffd5c4f04f35572c054925666756a4aeb
muradilova-dev <muradilova_r@iuca.kg>
2025-10-05 22:07:31 +0600

    Created forms for Post and Category

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Post, Category
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'categories']
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
